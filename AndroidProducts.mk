784b17fdbbae0f0bdb9cbccfbb83b0462e6a5e41
wudilsr <wudilsr@gmail.com>
2020-07-14 08:09:28 +0800

    wt88047: havoc bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_wt88047.mk
+    $(LOCAL_DIR)/havoc_wt88047.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_wt88047-user \
-    lineage_wt88047-userdebug \
-    lineage_wt88047-eng
+    havoc_wt88047-user \
+    havoc_wt88047-userdebug \
+    havoc_wt88047-eng
